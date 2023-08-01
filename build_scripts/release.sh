1a7f040194e5304dd809a14670cb895c2ee9e4f6
skill-issue0 <pratyushjain1020@gmail.com>
2023-08-01 19:03:18 +0530

    bruv 5

--- build_scripts/release.sh
+++ build_scripts/release.sh
@@ -1,20 +1,2 @@
-if [[ "$OSTYPE" != "darwin"* ]]; then
-    sudo apt install software-properties-common
-    sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-    sudo apt update
-    sudo apt install gcc-9 g++-9 -y
-    sudo apt install cmake -y
-
-    export CC="/usr/bin/gcc-9"
-    export CXX="/usr/bin/g++-9"
-fi
-
-mkdir -p bin/Release
-cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr/local -DLLVM_ENABLE_BACKTRACES=OFF \
--DLLVM_ENABLE_PER_TARGET_RUNTIME_DIR=OFF \
--DLLVM_ENABLE_TERMINFO=OFF \
--DLLVM_ENABLE_ZLIB=OFF \
--DLLVM_INCLUDE_EXAMPLES=OFF \
--DCMAKE_OSX_ARCHITECTURES="$ARCH" \
--DLLVM_INCLUDE_DOCS=OFF -DCMAKE_BUILD_TYPE=Release -DEXECUTABLE_OUTPUT_PATH="bin/Release" $@ .
-cmake --build . --config Release --verbose -- -j 3
+cd ../
+cargo build --release --features llvm14 --verbose
